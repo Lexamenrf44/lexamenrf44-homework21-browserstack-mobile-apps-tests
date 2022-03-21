de4d678713a3bd5ee6fc65682ef7d9bbe0ee2ebc
Lexamenrf44 <alexander.stcstudent@gmail.com>
2022-03-22 01:01:14 +0300

    edited Browserstack.java

--- src/test/java/com/browserstack/helpers/Browserstack.java
+++ src/test/java/com/browserstack/helpers/Browserstack.java
@@ -5,7 +5,7 @@ import static io.restassured.RestAssured.given;
 public class Browserstack {
     public static String videoUrl(String sessionId) {
         return given()
-                .auth().basic("andreyzavrichko_Xe3Bnj", "jhEPjqxKLx9dxaDjMSCk")
+                .auth().basic("lexamenrf_ZANKXG", "12m6SQ5ShDkCpCjS8euQ")
                 .when()
                 .get("https://api-cloud.browserstack.com/app-automate/sessions/" + sessionId +".json")
                 .then()
